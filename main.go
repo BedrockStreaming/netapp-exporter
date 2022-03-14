8a25a7155f544803b4040526cb8bbfa9dfb25f86
qbertrand.externe@m6.fr <qbertrand.externe@bedrockstreaming.com>
2022-03-14 15:55:17 +0100

    Feat: add target option in URL

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/BedrockStreaming/netapp-exporter/collector"
 
+	//"github.com/prometheus/exporter-toolkit/web"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gopkg.in/yaml.v2"
 )
@@ -23,38 +24,72 @@ type Config struct {
 	KnownHostsFile string `yaml:"known_hosts_file"`
 }
 
-func getConfigs(path string) []*Config {
-	var configs []*Config
-	yamlFile, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Printf("yamlFile.Get err #%v ", err)
-		os.Exit(1)
+var config []*Config
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	target := query.Get("target")
+	if len(query["target"]) != 1 || target == "" {
+		http.Error(w, "'target' parameter must be specified once", 400)
+		return
 	}
-	err = yaml.Unmarshal(yamlFile, &configs)
-	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
-		os.Exit(1)
+
+	hostExists := false
+	for _, config := range config {
+		if target == config.Host {
+			//hostExists = true
+			break
+		}
 	}
-	return configs
+	if !hostExists {
+		http.Error(w, "this target does not exist in config", 400)
+		return
+	}
+
+
+
+	// logger = log.With(logger, "target", target)
+	// level.Debug(logger).Log("msg", "Starting scrape")
+
+	// start := time.Now()
+	registry := prometheus.NewRegistry()
+	//c := collector.New(r.Context(), target, logger)
+	//registry.MustRegister(c)
+	// Delegate http serving to Prometheus client library, which will call collector.Collect.
+	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
+	// duration := time.Since(start).Seconds()
+	// level.Debug(logger).Log("msg", "Finished scrape", "duration_seconds", duration)
 }
 
 func main() {
-	configFile := flag.String("config", "config.yaml", "Config file path")
+	configPath := flag.String("config", "config.yaml", "Config path")
+	yamlFile, err := ioutil.ReadFile(*configPath)
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
 	listenAddress := flag.String("listenAddress", "0.0.0.0", "Expose prometheus metrics")
 	listenPort := flag.Int("listenPort", 9148, "Expose prometheus metrics")
 
 	flag.Parse()
 
-	configs := getConfigs(*configFile)
-
-	for _, config := range configs {
+	for _, config := range config {
 		collector.RecordMetrics(config.Username, config.Password, config.Host, config.Port, config.Interval, config.KnownHostsFile)
 	}
 
 	log.Println("Listening on " + *listenAddress + ":" + fmt.Sprint(*listenPort) + "/metrics")
 	http.Handle("/metrics", promhttp.Handler())
-	err := http.ListenAndServe(*listenAddress+":"+fmt.Sprint(*listenPort), nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	http.HandleFunc("/netapp", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Listening on " + *listenAddress + ":" + fmt.Sprint(*listenPort) + "/netapp")
+		handler(w, r)
+	})
+	//err := http.ListenAndServe(*listenAddress+":"+fmt.Sprint(*listenPort), nil)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 }
